9cf083dcb0da34afc9de80b1798609d621bf5e93
jeong yeong rok <glemfkcl@gmail.com>
2025-12-23 22:44:50 +0900

    courseops: force dynamic app layout

--- courseops/console/app/(app)/layout.tsx
+++ courseops/console/app/(app)/layout.tsx
@@ -3,9 +3,10 @@ import { redirect } from "next/navigation";
 import AppShell from "./ui/AppShell";
 import { getSession } from "@/lib/session";
 
+export const dynamic = "force-dynamic";
+
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
   if (!session) redirect("/login");
   return <AppShell userName={session.name}>{children}</AppShell>;
 }
-
